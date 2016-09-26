OpenDesireProject/android_device_htc_aceopt

libsensors: update sensorlist hammerhead style

libsensors/sensors.c
@@ -31,28 +31,66 @@
  */
 
 static const struct sensor_t sSensorList[] = {
-        { "BMA150 3-axis Accelerometer",
-                "Bosh",
-                1, SENSORS_HANDLE_BASE+ID_A,
-                SENSOR_TYPE_ACCELEROMETER, 4.0f*9.81f, (4.0f*9.81f)/256.0f, 0.2f, 0, 0, 0, SENSOR_STRING_TYPE_ACCELEROMETER, "", 0, 0, { } },
-        { "AK8975 3-axis Magnetic field sensor",
-                "Asahi Kasei",
-                1, SENSORS_HANDLE_BASE+ID_M,
-                SENSOR_TYPE_MAGNETIC_FIELD, 2000.0f, 1.0f/16.0f, 6.8f, 0, 0, 0, SENSOR_STRING_TYPE_MAGNETIC_FIELD, "", 0, 0, { } },
-        { "AK8975 Orientation sensor",
-                "Asahi Kasei",
-                1, SENSORS_HANDLE_BASE+ID_O,
-                SENSOR_TYPE_ORIENTATION, 360.0f, 1.0f, 7.0f, 0, 0, 0, SENSOR_STRING_TYPE_ORIENTATION, "", 0, 0, { } },
-        { "ISL29028 Proximity sensor",
-                "Intersil Corporation",
-                1, SENSORS_HANDLE_BASE+ID_P,
-                SENSOR_TYPE_PROXIMITY,
-                PROXIMITY_THRESHOLD_CM, PROXIMITY_THRESHOLD_CM,
-                0.5f, 0, 0, 0, SENSOR_STRING_TYPE_PROXIMITY, "", 0, 0, { } },
-        { "ISL29028 Light sensor",
-                "Intersil Corporation",
-                1, SENSORS_HANDLE_BASE+ID_L,
-                SENSOR_TYPE_LIGHT, 10240.0f, 1.0f, 0.5f, 0, 0, 0, SENSOR_STRING_TYPE_LIGHT, "", 0, 0, { } },
+    {
+        .name       = "BMA150 3-axis Accelerometer",
+        .vendor     = "Bosh",
+        .version    = 1,
+        .handle     = SENSORS_HANDLE_BASE+ID_A,
+        .type       = SENSOR_TYPE_ACCELEROMETER,
+        .maxRange   = 4.0f*9.81f,
+        .resolution = (4.0f*9.81f)/256.0f,
+        .power      = 0.2f,
+        .minDelay   = 0,
+        .reserved   = { }
+    },
+    {
+        .name       = "AK8975 3-axis Magnetic field sensor",
+        .vendor     = "Asahi Kasei",
+        .version    = 1,
+        .handle     = SENSORS_HANDLE_BASE+ID_M,
+        .type       = SENSOR_TYPE_MAGNETIC_FIELD,
+        .maxRange   = 2000.0f,
+        .resolution = 1.0f/16.0f,
+        .power      = 6.8f,
+        .minDelay   = 0,
+        .reserved   = { }
+    },
+    {
+        .name       = "AK8975 Orientation sensor",
+        .vendor     = "Asahi Kasei",
+        .version    = 1,
+        .handle     = SENSORS_HANDLE_BASE+ID_O,
+        .type       = SENSOR_TYPE_ORIENTATION,
+        .maxRange   = 360.0f,
+        .resolution = 1.0f,
+        .power      = 7.0f,
+        .minDelay   = 0,
+        .reserved   = { }
+    },
+    {
+        .name       = "ISL29028 Proximity sensor",
+        .vendor     = "Intersil Corporation",
+        .version    = 1,
+        .handle     = SENSORS_HANDLE_BASE+ID_P,
+        .type       = SENSOR_TYPE_PROXIMITY,
+        .maxRange   = PROXIMITY_THRESHOLD_CM,
+        .resolution = PROXIMITY_THRESHOLD_CM,
+        .power      = 0.5f,
+        .minDelay   = 0,
+        .reserved   = { }
+    },
+    {
+        .name       = "ISL29028 Light sensor",
+        .vendor     = "Intersil Corporation",
+        .version    = 1,
+        .handle     = SENSORS_HANDLE_BASE+ID_L,
+        .type       = SENSOR_TYPE_LIGHT,
+        .maxRange   = 10240.0f,
+        .resolution = 1.0f,
+        .power      = 0.5f,
+        .minDelay   = 0,
+        .reserved   = { }
+    },
 };
 
 static int open_sensors(const struct hw_module_t* module, const char* name,

bastei/android_device_htc_saga

Change-Id: I5f9c835e131c08258cfe689121702097b7aff2f6

libsensors/sensors.c
@@ -31,28 +31,66 @@
  */
 
 static const struct sensor_t sSensorList[] = {
-        { "BMA150 3-axis Accelerometer",
-                "Bosh",
-                1, SENSORS_HANDLE_BASE+ID_A,
-                SENSOR_TYPE_ACCELEROMETER, 4.0f*9.81f, (4.0f*9.81f)/256.0f, 0.2f, 0, 0, 0, "SENSOR_STRING_TYPE_ACCELEROMETER", "", 0, 0, { } },
-        { "AK8975 3-axis Magnetic field sensor",
-                "Asahi Kasei",
-                1, SENSORS_HANDLE_BASE+ID_M,
-                SENSOR_TYPE_MAGNETIC_FIELD, 2000.0f, 1.0f/16.0f, 6.8f, 0, 0, 0, "SENSOR_STRING_TYPE_MAGNETIC", "", 0, 0, { } },
-        { "AK8975 Orientation sensor",
-                "Asahi Kasei",
-                1, SENSORS_HANDLE_BASE+ID_O,
-                SENSOR_TYPE_ORIENTATION, 360.0f, 1.0f, 7.0f, 0, 0, 0, "SENSOR_STRING_TYPE_ORIENTATION", "", 0, 0, { } },
-        { "CM3628 Proximity sensor",
-                "Capella Microsystems",
-                1, SENSORS_HANDLE_BASE+ID_P,
-                SENSOR_TYPE_PROXIMITY,
-                PROXIMITY_THRESHOLD_CM, PROXIMITY_THRESHOLD_CM,
-                0.5f, 0, 0, 0, "SENSOR_STRING_TYPE_PROXIMITY", "", 0, 0, { } },
-        { "CM3628 Light sensor",
-                "Capella Microsystems",
-                1, SENSORS_HANDLE_BASE+ID_L,
-                SENSOR_TYPE_LIGHT, 10240.0f, 1.0f, 0.5f, 0, 0, 0, "SENSOR_STRING_TYPE_LIGHT", "", 0, 0, { } },
+    {
+        .name       = "BMA150 3-axis Accelerometer",
+        .vendor     = "Bosh",
+        .version    = 1,
+        .handle     = SENSORS_HANDLE_BASE+ID_A,
+        .type       = SENSOR_TYPE_ACCELEROMETER,
+        .maxRange   = 4.0f*9.81f,
+        .resolution = (4.0f*9.81f)/256.0f,
+        .power      = 0.2f,
+        .minDelay   = 0,
+        .reserved   = { }
+    },
+    {
+        .name       = "AK8975 3-axis Magnetic field sensor",
+        .vendor     = "Asahi Kasei",
+        .version    = 1,
+        .handle     = SENSORS_HANDLE_BASE+ID_M,
+        .type       = SENSOR_TYPE_MAGNETIC_FIELD,
+        .maxRange   = 2000.0f,
+        .resolution = 1.0f/16.0f,
+        .power      = 6.8f,
+        .minDelay   = 0,
+        .reserved   = { }
+    },
+    {
+        .name       = "AK8975 Orientation sensor",
+        .vendor     = "Asahi Kasei",
+        .version    = 1,
+        .handle     = SENSORS_HANDLE_BASE+ID_O,
+        .type       = SENSOR_TYPE_ORIENTATION,
+        .maxRange   = 360.0f,
+        .resolution = 1.0f,
+        .power      = 7.0f,
+        .minDelay   = 0,
+        .reserved   = { }
+    },
+    {
+        .name       = "CM3628 Proximity sensor",
+        .vendor     = "Capella Microsystems",
+        .version    = 1,
+        .handle     = SENSORS_HANDLE_BASE+ID_P,
+        .type       = SENSOR_TYPE_PROXIMITY,
+        .maxRange   = PROXIMITY_THRESHOLD_CM,
+        .resolution = PROXIMITY_THRESHOLD_CM,
+        .power      = 0.5f,
+        .minDelay   = 0,
+        .reserved   = { }
+    },
+    {
+        .name       = "CM3628 Light sensor",
+        .vendor     = "Capella Microsystems",
+        .version    = 1,
+        .handle     = SENSORS_HANDLE_BASE+ID_L,
+        .type       = SENSOR_TYPE_LIGHT,
+        .maxRange   = 10240.0f,
+        .resolution = 1.0f,
+        .power      = 0.5f,
+        .minDelay   = 0,
+        .reserved   = { }
+    },
 };
 
 static int open_sensors(const struct hw_module_t* module, const char* name,